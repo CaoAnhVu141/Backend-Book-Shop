17bece325396a9ecfdb097065d3570baf95135c2
caoanhvu <caovu5541@gmail.com>
2025-09-06 22:33:10 +0700

    commit code

---
+++ src/review/review.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ReviewService } from './review.service';
+import { ReviewController } from './review.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Review,ReviewSchema } from './schemas/review.schema';
+
+@Module({
+  controllers: [ReviewController],
+  providers: [ReviewService],
+  imports: [MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }])],
+})
+export class ReviewModule {}
---
+++ src/review/schemas/review.schema.ts
@@ -0,0 +1,51 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+
+export type ReviewDocument = HydratedDocument<Review>;
+
+@Schema({timestamps: true})
+export class Review {
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    user: mongoose.Schema.Types.ObjectId;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Book' })
+    book: mongoose.Schema.Types.ObjectId;
+
+    @Prop()
+    comment: string;
+
+    images: [{ type: string }];
+
+    @Prop({ type: Object })
+    createdBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    }
+
+    @Prop({ type: Object })
+    updatedBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    }
+
+    @Prop({ type: Object })
+    deletedBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    }
+
+    @Prop()
+    createdAt: Date;
+
+    @Prop()
+    updatedAt: Date;
+
+    @Prop()
+    isDeleted: boolean;
+
+    @Prop()
+    deletedAt: Date;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(Review);
