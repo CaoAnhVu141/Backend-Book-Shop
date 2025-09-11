25e5d7fa3195fa98417eb05df6ea510a549b3357
caoanhvu <caovu5541@gmail.com>
2025-09-11 17:14:50 +0700

    commit schema order-item

--- src/order-items/order-items.module.ts
+++ src/order-items/order-items.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrderItemsService } from './order-items.service';
 import { OrderItemsController } from './order-items.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Book, BookSchema } from 'src/books/schemas/book.schema';
+import { Order, OrderSchema } from 'src/orders/schemas/order.schema';
+import { OrderItem,OrderItemSchema } from './schemas/order-item.shema';
 
 @Module({
   controllers: [OrderItemsController],
-  providers: [OrderItemsService]
+  providers: [OrderItemsService],
+  imports: [MongooseModule.forFeature([{ name: OrderItem.name, schema: OrderItemSchema },
+      {name: Order.name, schema: OrderSchema},
+      {name: Book.name, schema: BookSchema}
+    ])],
 })
 export class OrderItemsModule {}
--- src/order-items/schemas/order-item.shema.ts
+++ src/order-items/schemas/order-item.shema.ts
@@ -1,1 +1,33 @@
-export class OrderItem {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+
+export type OrderItemDocument = HydratedDocument<OrderItem>;
+
+@Schema({ timestamps: true })
+export class OrderItem {
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true })
+    order: mongoose.Schema.Types.ObjectId;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true })
+    book: mongoose.Schema.Types.ObjectId;
+
+    @Prop()
+    nameBook: string;
+
+    @Prop({ required: true })
+    quantity: number; // số lượng sách
+
+    @Prop({ required: true })
+    price: number;
+
+    @Prop({ default: 0 })
+    discount: number;
+
+    @Prop({ required: true })
+    totalPrice: number;
+
+    @Prop({ default: Date.now })
+    createdAt: Date;
+}
+export const OrderItemSchema = SchemaFactory.createForClass(OrderItem);
