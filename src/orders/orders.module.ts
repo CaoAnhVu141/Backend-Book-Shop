29856c6d48ff32c779c63794a7af8a4da5bec1dc
caoanhvu <caovu5541@gmail.com>
2025-09-11 17:00:51 +0700

    update schema order

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
+  imports: [MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema },
+      {name: User.name, schema: UserSchema},
+    ])],
 })
 export class OrdersModule {}
--- src/orders/schemas/order.schema.ts
+++ src/orders/schemas/order.schema.ts
@@ -1,21 +1,79 @@
-import { Prop } from "@nestjs/mongoose";
-import mongoose from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
 
+export type OrderDocument = HydratedDocument<Order>;
+
+@Schema({ timestamps: true })
 export class Order {
+
+    @Prop({ required: true })
+    orderCode: string;
 
     @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
     user: mongoose.Schema.Types.ObjectId;
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Book' })
-    book: mongoose.Schema.Types.ObjectId;
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Address' })
+    address: mongoose.Schema.Types.ObjectId;
 
-    @Prop()
-    totalAmount: number;
+    @Prop({ required: true })
+    totalAmount: number; // tổng tiền
 
-    totalQuantity: number;
+    @Prop({ required: true })
+    totalQuantity: number;  // tổng số lượng
 
+    @Prop({ required: true })
     orderDate: Date;
+
+    @Prop({ default: 0 })
+    discountAmount: number; 
 
     @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' })
     payment: mongoose.Schema.Types.ObjectId;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Coupon' })
+    coupon: mongoose.Schema.Types.ObjectId;
+
+    @Prop({ required: true, enum: ['Shipped', 'In Transit', 'Delivered'] })
+    shippingStatus: string;
+
+    @Prop({
+        required: true,
+        enum: ['Pending', 'Confirmed', 'Shipping', 'Delivered', 'Cancelled'],
+        default: 'Pending'
+    })
+    orderStatus: string;
+
+    @Prop({ required: true, enum: ['Pending', 'Completed', 'Failed'] })
+    paymentStatus: string;
+
+    @Prop({ type: Object })
+    createdBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    }
+
+    @Prop({ type: Object })
+    updatedBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    }
+
+    @Prop({ type: Object })
+    deletedBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    }
+
+    @Prop()
+    createdAt: Date;
+
+    @Prop()
+    updatedAt: Date;
+
+    @Prop()
+    isDeleted: boolean;
+
+    @Prop()
+    deletedAt: Date;
 }
+export const OrderSchema = SchemaFactory.createForClass(Order);
