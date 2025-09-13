b3f6c20e8fe490f9ff5b781c448e4de3a130b322
caoanhvu <caovu5541@gmail.com>
2025-09-13 20:55:50 +0700

    commit point

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PointService } from './point.service';
 import { PointController } from './point.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Point, PointSchema } from './schemas/point.schema';
 
 @Module({
   controllers: [PointController],
-  providers: [PointService]
+  providers: [PointService],
+  imports: [MongooseModule.forFeature([{ name: Point.name, schema: PointSchema }])],
 })
 export class PointModule {}
---
+++ src/point/schemas/point.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+
+export type OrderItemDocument = HydratedDocument<Point>;
+
+@Schema({ timestamps: true })
+
+export class Point {
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    user: mongoose.Schema.Types.ObjectId;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: false  })
+    order: mongoose.Schema.Types.ObjectId | null;
+
+    @Prop({ required: true })
+    point: number;
+
+    @Prop({ required: true })
+    source: string; // type of coint
+
+    @Prop({ required: true })
+    isActive: boolean;
+
+    @Prop({ required: true })
+    createdAt: Date;
+
+    @Prop({ required: true })
+    updatedAt: Date;
+}
+export const PointSchema = SchemaFactory.createForClass(Point);
